03246bdcbd86559e59b2c485252be103940b49e2
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2025-02-06 10:56:00 +0530

    [Rushikesh]: Adding part time Employee and wage assuming part time hour is 6hrs

--- EmpWage.py
+++ EmpWage.py
@@ -6,7 +6,13 @@ def check_attendance(attendance):
         return 0
 
     if attendance == 1:
-        return 20*8
+        time = input("Enter type of job (part/full): ").lower()
+
+        if time == "part":
+            return 20*6
+        
+        elif time == "full":
+            return 20*8
 
 attendance_check=random.randint(0,1)
 print(f"The wage is: {check_attendance(attendance_check)}")
