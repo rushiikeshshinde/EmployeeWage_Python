52b41fd16c26d458f96a944dd791e9d7b94c4023
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2025-02-06 11:00:34 +0530

    [Rushikesh]: Solving the same use case using Match Case

--- EmpWage.py
+++ EmpWage.py
@@ -2,17 +2,22 @@ import random
 
 def check_attendance(attendance):
     
-    if attendance == 0:
-        return 0
-
-    if attendance == 1:
-        time = input("Enter type of job (part/full): ").lower()
-
-        if time == "part":
-            return 20*6
+    match attendance:
+        case 0:
+            return 0
         
-        elif time == "full":
-            return 20*8
+        case 1:
+            time = input("Enter a type of job (part/full) :").lower()
+
+            match time:                
+                case "part":
+                    return 20 * 6
+                
+                case "full":
+                    return 20 * 8
+                
+                case _:
+                    return "Invalid job type"
 
 attendance_check=random.randint(0,1)
 print(f"The wage is: {check_attendance(attendance_check)}")
